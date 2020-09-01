fae00878f432fbcc1bc8a7befdcd45c876b2ad51
NatalyMak <49818793+nat081018@users.noreply.github.com>
2020-09-01 13:50:45 +0300

    added  node-sass  and  created the project structure

---
+++ src/components/footer/footer.component.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import "./footer.style.scss"
+
+const Footer  = () => (
+    <div className="footer__wrapper">
+        <p>2020 all right reserved</p>
+    </div>
+)
+
+export default Footer
