d3e2289ae3e664ad19dbfe4cb7dd47e4d2fbfc33
NatalyMak <49818793+nat081018@users.noreply.github.com>
2020-09-24 16:05:04 +0300

    add React.lazy

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, {lazy, Suspense} from 'react';
 import './App.css';
 
 import Header from "./components/header/header.component"
-import CurrencyExchangeTable from "./components/currency_exchange_table/currency_exchange_table.component"
+// import CurrencyExchangeTable from "./components/currency_exchange_table/currency_exchange_table.component"
 import Footer from "./components/footer/footer.component"
+
+const CurrencyExchangeTable = React.lazy(() => import("./components/currency_exchange_table/currency_exchange_table.component") )
 
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <CurrencyExchangeTable />
+      <Suspense fallback={<div>Loading...</div>}>
+        <CurrencyExchangeTable />
+      </Suspense>
       <Footer/>
     </div>
   );
